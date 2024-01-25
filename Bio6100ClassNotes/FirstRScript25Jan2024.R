fee40a559cdc68e3ec1fc01a5050a9dbb3c82876
NicoleRGorman <144243241+NicoleRGorman@users.noreply.github.com>
2024-01-25 15:26:25 -0500

    Initial commit

---
+++ Bio6100ClassNotes/FirstRScript25Jan2024.R
@@ -0,0 +1,16 @@
+#First comment to explain what this program is doin
+# Be expansive and describe in great detail This may seem trivial, but needs to happen
+# Simple script to examine the distribution of the produce of two unifor  vaiatbale
+#. 25 january 2024
+# NRG
+
+# Premilinaries
+library(ggplot2)
+set.seed(100)
+library(TeachingDemos) #use this to set the random number seed from a chgaracter string
+char2seed("green tea")
+char2seed("green tea", set=FALSE)
+#
+
+#Global variables
+ranVar1
